c94654cbbe7dec3fa93a590b348bcb9d6ba97424
miri <1791863590@qq.com>
2026-06-22 14:32:47 +0800

    后端初始化

---
+++ src/main/java/com/miri/aibuilderbackend/AibuilderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.miri.aibuilderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AibuilderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AibuilderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miri/aibuilderbackend/AibuilderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miri.aibuilderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AibuilderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
