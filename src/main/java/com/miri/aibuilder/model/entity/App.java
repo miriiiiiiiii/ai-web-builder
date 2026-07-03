ca6f4d0cdf61de5e7db2ac7c50089197485c7096
miri <1791863590@qq.com>
2026-07-03 14:05:54 +0800

    应用模块（完成基本功能） 1.用cursor仿照已有的用户模块生成基础功能代码 2.完成基本功能、swagger测试通过

--- src/main/java/com/miri/aibuilder/model/entity/App.java
+++ src/main/java/com/miri/aibuilder/model/entity/App.java
@@ -4,6 +4,7 @@ import com.mybatisflex.annotation.Column;
 import com.mybatisflex.annotation.Id;
 import com.mybatisflex.annotation.KeyType;
 import com.mybatisflex.annotation.Table;
+import com.mybatisflex.core.keygen.KeyGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,7 +32,7 @@ public class App implements Serializable {
     /**
      * id
      */
-    @Id(keyType = KeyType.Auto)
+    @Id(keyType = KeyType.Generator, value = KeyGenerators.snowFlakeId)
     private Long id;
 
     /**
--- src/main/java/com/miri/aibuilder/service/AppService.java
+++ src/main/java/com/miri/aibuilder/service/AppService.java
@@ -1,7 +1,12 @@
 package com.miri.aibuilder.service;
 
+import com.miri.aibuilder.model.dto.app.AppQueryRequest;
 import com.miri.aibuilder.model.entity.App;
+import com.miri.aibuilder.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -10,4 +15,24 @@ import com.mybatisflex.core.service.IService;
  */
 public interface AppService extends IService<App> {
 
+    /**
+     * 获取应用封装类
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 构造应用查询条件
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    /**
+     * 获取应用封装类列表
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
 }
